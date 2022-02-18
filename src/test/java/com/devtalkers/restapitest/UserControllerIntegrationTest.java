2881ab349ba7b7d33135aa5a680bc2e0a4806d04
cmsjulio <cms.julio1@gmail.com>
2022-02-18 17:03:21 -0300

    Rodando teste com @SpringBootTest

--- src/test/java/com/devtalkers/restapitest/UserControllerIntegrationTest.java
+++ src/test/java/com/devtalkers/restapitest/UserControllerIntegrationTest.java
@@ -1,2 +1,33 @@
-package com.devtalkers.restapitest;public class UserControllerIntegrationTest {
+package com.devtalkers.restapitest;
+
+import com.devtalkers.restapitest.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @Sql("/test.sql")
+    public void getUserByIdTest(){
+
+        ResponseEntity<User> response = testRestTemplate.getForEntity("/user/1001", User.class);
+
+        assertEquals(1001, response.getBody().getId());
+        assertEquals("John", response.getBody().getName());
+        assertEquals("John@com.com", response.getBody().getEmail());
+        assertEquals("987654321", response.getBody().getPhone());
+        assertEquals("male", response.getBody().getGender());
+
+    }
+
 }
---
+++ src/test/resources/test.sql
@@ -0,0 +1,2 @@
+insert into user (ID, NAME, EMAIL, PHONE, GENDER) values (1001, 'John', 'John@com.com', '987654321','male')
+insert into user (ID, NAME, EMAIL, PHONE, GENDER) values (1002, 'Mary', 'Mary@com.com', '987654322','female')
